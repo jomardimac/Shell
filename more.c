10d59b0fedf83a43a9f7aa3b2c77f4bc1e410057
Jomar <jomaridimaculangan@gmail.com>
2017-12-02 18:34:32 -0800

    fix more

--- more.c
+++ more.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
     int n = 0;
     int fd;
 
-    char buf[1028], line[64];
+    char buf[1028], cmdline[64];
     if(argc < 0){
         prints("Nothing inputted\n");
 
@@ -16,7 +16,16 @@ int main(int argc, char *argv[]){
 
     }
     else{
-        
+        fd = open(argv[1], 0);
+        while(n = (read(fd, buf, 1028)){
+            printf("%s", buf);
+            
+            if(n == 80){
+                prints("\n");
+                getc();
+                
+            }
+        }
     }
 }
 #endif
